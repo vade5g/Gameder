b0961ab8e79abfcdc6f532cff259ffadfc3173d5
Aidar Mukhamadiev <noiresonn@gmail.com>
2017-04-24 14:51:31 +0300

    Add server constituent

---
+++ server/src/main/java/com/gameder/app/Server.java
@@ -0,0 +1,12 @@
+package com.gameder.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+}
