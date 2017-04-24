a71212064a8872e29f0302b914494c96f0fc3a51
Aidar Mukhamadiev <noiresonn@gmail.com>
2017-04-24 14:51:31 +0300

    Add server constituent

---
+++ server/src/main/java/com/gameder/app/handlers/gamers/Gamer.java
@@ -0,0 +1,13 @@
+package com.gameder.app.handlers.gamers;
+
+public class Gamer {
+    private final long id;
+
+    public Gamer() {
+        this.id = 1;
+    }
+
+    public long getId() {
+        return id;
+    }
+}
