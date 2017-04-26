a53980003122c0f8c2ad2b8878d1876f18edc0a8
vade5g <valtteri.kukkola@gmail.com>
2017-04-26 10:57:19 +0300

    Still merging from aelittas version.

---
+++ client/app/components/TextInput.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class TextInput extends Component{
+  constructor(props) {
+    super(props);
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      value: props.value,
+    }
+  }
+
+  onInputChange(ev) {
+    this.setState({
+      value: ev.target.value,
+    });
+  }
+
+  render(){
+    const {
+      className,
+      value,
+      placeholder,
+      label,
+    } = this.props;
+    const input = (
+      <input type="text" className={className} value={this.state.value} onChange={this.onInputChange} placeholder={placeholder}/>
+    );
+
+    if(label)
+      return(
+        <div>
+          <label className='control-label'>{label}</label>
+          {input}
+        </div>
+      );
+    else{
+      return input;
+    }
+  }
+}
+
+TextInput.defaultProps = {
+  className: 'form-control',
+  value: '',
+  placeholder: '',
+};
+
+TextInput.propTypes = {
+  className: PropTypes.string,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  label: PropTypes.string,
+};
