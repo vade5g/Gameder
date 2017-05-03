c681ccafeabfbbdb973a5f1a83335e721f255e59
Aelitta Ezugbaya <aelittae@gmail.com>
2017-05-03 10:58:47 +0300

    Changed position of buttons/Images have the same height now

--- client/app/components/UserView.js
+++ client/app/components/UserView.js
@@ -9,15 +9,16 @@ const UserShape = {
 
 class UserView extends Component{
   render(){
-    console.log(this.props)
     const username = this.props.user.username;
     return(
       <div >
-        <img src={`/assets/accounts/${username}/avatar.jpg`} className="img-rounded img-responsive center-block" alt="Cinque Terre" width="300" height="500"/>
+        <div className='img-view'>
+          <img src={`/assets/accounts/${username}/avatar.jpg`} className="img-rounded img-responsive center-block" alt="Cinque Terre"/>
+        </div>
         <div className="center">
         <h3>{`Username: ${username}`}</h3>
-          <Button className='btn-circle yes' style="success" onClick={this.props.onSuccessClick}>Yes</Button>
           <Button className='btn-circle no' style="danger" onClick={this.props.onDenyClick}>No</Button>
+          <Button className='btn-circle yes' style="success" onClick={this.props.onSuccessClick}>Yes</Button>
         </div>
       </div>
     );
