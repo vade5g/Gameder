ea3421e56d368068731acb03808453ada89af316
Aelitta Ezugbaya <aelittae@gmail.com>
2017-05-07 21:55:55 +0300

    Two new broser tests :tada:

--- client/test/specs/newTest.js
+++ client/test/specs/newTest.js
@@ -16,5 +16,47 @@ describe('Discovery page tests', function() {
       browser.call(done);
     });
   });
+  describe('Picture should have username', function() {
+    it('Username is under picture', function(done) {
+      browser
+        .waitForExist('.username');
+
+      let username = browser.getText('.username').substring(10);
+
+      expect(browser.getText('.username').length)
+        .toBeGreaterThan(0);
+
+      browser.call(done);
+    });
+  });
+
+  describe('Actions after click on "Yes" or "No"', function() {
+    it('After pressing "Yes" another profile is showed', function(done) {
+      browser
+        .waitForExist('.username');
+
+      let usernameBefore = browser.getText('.username').substring(10);
+      browser.click(".yes");
+      let usernameAfter = browser.getText('.username').substring(10);
+
+      expect(usernameBefore === usernameAfter)
+        .toBe(false);
+
+      browser.call(done);
+    });
+    it('After pressing "No" another profile is showed', function(done) {
+      browser
+        .waitForExist('.username');
+
+      let usernameBefore = browser.getText('.username').substring(10);
+      browser.click(".no");
+      let usernameAfter = browser.getText('.username').substring(10);
+
+      expect(usernameBefore === usernameAfter)
+        .toBe(false);
+
+      browser.call(done);
+    });
+  });
 
 });
